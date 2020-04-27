223e4c65e719abc377146c4d80730bab100413f8
danbrud <dannybrudner@users.noreply.github.com>
2020-04-27 22:49:54 +0300

    client: styled contract and added to clients add page

--- src/components/Admin/Clients/AddClient.tsx
+++ src/components/Admin/Clients/AddClient.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { TextField, Button, makeStyles } from '@material-ui/core'
 import { useClientsStore } from '../../../context/Clients.context'
-import AddContract from '../AddContract'
+import AddContract from './AddContract'
 import { AddItemProps } from '../AddItemProps.interface'
 import { checkRequiredFields } from '../../../utils'
 
@@ -10,6 +10,9 @@ const useStyles = makeStyles((theme) => ({
   button: {
     marginTop: '10px',
     color: 'white'
+  },
+  buttonOutlined: {
+    marginTop: '10px',
   },
   input: {
     margin: '2px'
@@ -24,6 +27,7 @@ const AddClient: React.FC<AddItemProps> = (props) => {
     name: '', phone: '', email: '', spouseName: '',
     address: '', city: '', description: ''
   })
+  const [hasContract, setHasContract] = useState(false)
 
   const handleChange = ({ target }) => {
     setInputs({ ...inputs, [target.name]: target.value })
@@ -40,18 +44,20 @@ const AddClient: React.FC<AddItemProps> = (props) => {
     const requiredFields = ['name', 'phone', 'email', 'address', 'city']
     if (!checkRequiredFields(requiredFields, inputs)) {
       props.openSnackbar('error', 'Invalid! Make sure to fill all inputs.')
-      return
+      return false
     }
 
     try {
       await ClientsStore.addClient(inputs)
     } catch (e) {
       props.openSnackbar('error', 'Error! Something went wrong, try again!')
-      return
+      return false
     }
 
     props.openSnackbar('success', 'Added client successfully!')
     clearInputs()
+
+    return true
   }
 
   return (
@@ -124,18 +130,41 @@ const AddClient: React.FC<AddItemProps> = (props) => {
           type='text'
           onChange={handleChange}
         />
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          fullWidth={true}
-          onClick={handleSubmit}
-        >
-          ADD CLIENT
-        </Button>
-        <AddContract />
+        {
+          !hasContract
+            ? (
+              <div>
+                < Button
+                  className={classes.buttonOutlined}
+                  variant="outlined"
+                  color="primary"
+                  fullWidth={true}
+                  onClick={() => setHasContract(true)}
+                >
+                  ADD CONTRACT
+                </Button>
+                < Button
+                  className={classes.button}
+                  variant="contained"
+                  color="primary"
+                  fullWidth={true}
+                  onClick={handleSubmit}
+                >
+                  ADD CLIENT
+                </Button>
+              </div>
+            )
+            : <AddContract
+              clientName={inputs.name}
+              openSnackbar={props.openSnackbar}
+              clientPage={true}
+              submitClient={handleSubmit}
+              setHasContract={setHasContract}
+            />
+        }
+
       </div>
-    </div>
+    </div >
   )
 }
 
---
+++ src/components/Admin/Clients/AddContract.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react'
+import { useGeneralAdminStore } from '../../../context/GeneralAdmin.context'
+import { useClientsStore } from '../../../context/Clients.context'
+import { observer } from 'mobx-react'
+import { AddItemProps } from '../AddItemProps.interface'
+import { removeOptionalFields } from '../../../utils'
+import { Button, makeStyles, TextField } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    marginTop: '10px',
+    color: 'white'
+  },
+  buttonOutlined: {
+    marginTop: '10px',
+  },
+  input: {
+    margin: '2px'
+  }
+}))
+
+interface AddContractProps extends AddItemProps {
+  clientPage?: boolean
+  submitClient?: () => Promise<boolean>
+  setHasContract?: (hasContract: boolean) => void
+}
+
+const AddContract: React.FC<AddContractProps> = observer((props) => {
+  const classes = useStyles()
+  const GeneralAdminStore = useGeneralAdminStore()
+  const ClientsStore = useClientsStore()
+
+  const { clientName, submitClient, clientPage, openSnackbar, setHasContract } = props
+  const [contract, setContract] = useState({})
+  const [price, setPrice] = useState('')
+
+  const handleChange = ({ target }) => {
+    setContract({ ...contract, [target.name]: target.value })
+  }
+
+  const handleSubmit = async () => {
+    const filledInputs = Object.keys(contract)
+    const inputs = removeOptionalFields(filledInputs, contract)
+    if (!filledInputs.length || !price) {
+      openSnackbar('error', 'Invalid! Make sure to fill all contract inputs.')
+      return
+    }
+
+    if (clientPage) {
+      const success = await submitClient()
+      if (!success) {
+        openSnackbar('error', 'Invalid! Make sure to fill all client inputs.')
+        return
+      }
+    }
+
+    for (let c in inputs) {
+      inputs[c] = parseInt(inputs[c])
+    }
+
+    try {
+      const client = ClientsStore.getClientByName(clientName)
+      await client.updateClient('pricePerHour', parseInt(price))
+      await client.addContract(inputs)
+    } catch (e) {
+      openSnackbar('error', 'Error! Something went wrong with adding contract, try again!')
+      return
+    }
+
+    const message = clientPage
+      ? 'Added contract & contract successfully!'
+      : 'Added contract successfully!'
+    openSnackbar('success', message)
+    clearInputs()
+  }
+
+  const handleHide = () => {
+    clearInputs()
+    setHasContract(false)
+  }
+
+  const clearInputs = () => {
+    const inputs = { ...contract }
+    for (let key in contract) {
+      inputs[key] = ''
+    }
+
+    setContract(inputs)
+    setPrice('')
+  }
+
+  return (
+    <div>
+      <TextField
+        className={classes.input}
+        fullWidth={true}
+        required={true}
+        label='Price Per Hour'
+        type='number'
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      {GeneralAdminStore.services.map(s => (
+        <TextField
+          key={s.id}
+          className={classes.input}
+          fullWidth={true}
+          label={`Included Hours for ${s.name}`}
+          type='number'
+          value={contract[s.id]}
+          onChange={handleChange}
+        />
+      ))}
+      < Button
+        className={classes.button}
+        variant="contained"
+        color="primary"
+        fullWidth={true}
+        onClick={handleSubmit}
+      >
+        ADD {clientPage ? 'CLIENT &' : null} CONTRACT
+      </Button>
+      < Button
+        className={classes.buttonOutlined}
+        variant="outlined"
+        color="primary"
+        fullWidth={true}
+        onClick={handleHide}
+      >
+        HIDE CONTRACT
+      </Button>
+    </div>
+  )
+})
+
+export default AddContract
