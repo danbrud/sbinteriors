81f091e3bf06bb122791b6ab21a8da6c7e0c34c2
danbrud <dannybrudner@users.noreply.github.com>
2020-04-29 21:42:07 +0300

    client: added loader to clients page

--- src/components/Admin/Clients/Clients.tsx
+++ src/components/Admin/Clients/Clients.tsx
@@ -3,27 +3,41 @@ import { observer } from 'mobx-react'
 import ClientCard from './ClientCard'
 import { useClientsStore } from '../../../context/Clients.context'
 import TextField from '@material-ui/core/TextField'
+import Loader from '../../Loader'
+import NoData from '../NoData'
 
 
 const Clients: React.FC = observer(() => {
-  const [input, setInput] = useState<string>('')
+  const [input, setInput] = useState('')
   const ClientsStore = useClientsStore()
+  const [isLoading, setIsLoading] = useState(ClientsStore.isPopulated ? false : true)
+
+  const initializeComp = async () => {
+    if (!ClientsStore.isPopulated) {
+      await ClientsStore.getClientsFromDB()
+      setIsLoading(false)
+    }
+  }
 
   useEffect(() => {
-    if (!ClientsStore.isPopulated) {
-      ClientsStore.getClientsFromDB()
-    }
+    initializeComp()
   }, [])
 
   return (
-    <div>
-      <TextField fullWidth={true} label="Search Clients" onChange={e => setInput(e.target.value)} />
-      {
-        ClientsStore.clients
-          .filter(c => c.name.toLowerCase().includes(input.toLowerCase()))
-          .map(client => <ClientCard key={client.id} client={client} />)
-      }
-    </div>
+    isLoading
+      ? <Loader />
+      : !ClientsStore.isPopulated
+        ? <NoData type='clients' />
+        : (
+          <div>
+            <TextField fullWidth={true} label="Search Clients" onChange={e => setInput(e.target.value)} />
+            {
+              ClientsStore.clients
+                .filter(c => c.name.toLowerCase().includes(input.toLowerCase()))
+                .map(client => <ClientCard key={client.id} client={client} />)
+            }
+          </div>
+        )
   )
 })
 
